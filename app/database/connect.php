9640fa0cc62a7f1ecafa1bdc5c4118066b1bcf93
Risa Goto <omusubi5656dup@gmail.com>
2025-03-28 22:46:32 +0900

    add:phpadminと接続

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,5 +1,12 @@
 <?php
 //DBと接続
-$pdo = new PDO('mysql:host=localhost;dbname=test',$user, $pass);
+$user = 'user';
+$pass = 'password';
 
+try{
+$pdo = new PDO('mysql:host=db;dbname=php_2chan',$user, $pass);
+echo '接続に成功しました';
+}catch(PDOException $error){
+    echo $error->getMessage();
+}
 ?>
