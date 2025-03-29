4c2a7a7211e06f83047f7b866b9b163307944cac
Risa Goto <omusubi5656dup@gmail.com>
2025-03-29 15:40:15 +0900

    add:エスケープ処理

--- index.php
+++ index.php
@@ -10,10 +10,16 @@ if(isset($_POST['submitButton'])){
     //名前入力チェック
     if(empty($_POST['username'])){
         $error_message['username'] = '名前を入力してください。';
+    }else {
+        //エスケープ処理
+        $escaped["username"] = htmlspecialchars($_POST['username'], ENT_QUOTES,"UTF-8");
     }
     //コメント入力チェック
     if(empty($_POST['body'])){
         $error_message['body'] = 'コメントを入力してください。';
+    }else {
+        //エスケープ処理
+        $escaped["body"] = htmlspecialchars($_POST['body'], ENT_QUOTES,"UTF-8");
     }
 
     if(empty($error_message)){
@@ -22,8 +28,8 @@ if(isset($_POST['submitButton'])){
         $sql = "INSERT INTO comment (username, body, post_date) VALUES (:username, :body, :post_date)";
         $statement = $pdo->prepare($sql);
         //値をセットする
-        $statement->bindParam(':username', $_POST['username'],PDO::PARAM_STR);
-        $statement->bindParam(':body', $_POST['body'],PDO::PARAM_STR);
+        $statement->bindParam(':username', $escaped["username"],PDO::PARAM_STR);
+        $statement->bindParam(':body', $escaped["body"],PDO::PARAM_STR);
         $statement->bindParam(':post_date', $post_date, PDO::PARAM_STR);
         $statement->execute();
     }
